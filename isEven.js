df7fa6c07284a3f9a116b82b939c39872839f03e
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-31 14:25:42 +0600

    Even number

---
+++ isEven.js
@@ -0,0 +1,17 @@
+console.log(4/2)
+console.log(158/2)
+console.log(552/2)
+console.log(742/2)
+console.log(326/2)
+
+function isEven(number){
+    if(number % 2 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+var number = 0;
+var cheekEven = isEven(number)
+console.log(cheekEven)
